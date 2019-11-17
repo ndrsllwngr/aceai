49304a8ede8e87e5b3274fdabc8d3a9ad1191e5a
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2019-11-17 22:46:09 +0100

    Fix layout

--- site/src/components/layout.js
+++ site/src/components/layout.js
@@ -8,8 +8,9 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { ButtonAppBar } from './appbar';
 
-import Header from "./header"
+// import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -25,11 +26,12 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+      <ButtonAppBar />
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          // maxWidth: 960,
           padding: `0px 1.0875rem 1.45rem`,
           paddingTop: 0,
         }}
--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -3,17 +3,16 @@ import React from "react"
 
 // import Image from "../components/image"
 import SEO from "../components/seo"
+import Layout from '../components/layout';
 
 import { PoseNetCamera } from "../components/PoseNetCamera/camera";
-import { ButtonAppBar } from '../components/appbar';
 
 const IndexPage = () => (
-  <>
-    <ButtonAppBar />
+  <Layout>
     <PoseNetCamera />
     <SEO title="Body posture" />
     {/* <Link to="/page-2/">Go to page 2</Link> */}
-  </>
+  </Layout>
 )
 
 export default IndexPage
