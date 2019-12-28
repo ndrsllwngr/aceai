b44c892cebd4574c95bbe9e6d5a587edadd38b0a
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2019-12-28 17:43:38 +0100

    Add toaster proof of concept

--- site/src/components/pages/Dashboard/Dashboard.js
+++ site/src/components/pages/Dashboard/Dashboard.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Toaster, Button, Intent } from '@blueprintjs/core';
 import SEO from '../../seo';
 import { PoseNetCamera } from '../../PoseNetCamera/camera';
 // import { useApp } from '../ctx-app';
 
 export const Dashboard = () => {
   // const [appContext] = useApp();
-
+  const toasterRef = useRef(null);
+  const showToast = () => {
+    if (toasterRef.current) {
+      toasterRef.current.show({ message: 'test', intent: Intent.PRIMARY });
+    }
+  };
   return (
     <>
       <SEO title="Dashboard" />
+      <Button onClick={showToast}>Test</Button>
+      <Toaster
+        autoFocus={false}
+        canEscapeKeyClear
+        position="bottom-right"
+        ref={toasterRef}
+      ></Toaster>
       <PoseNetCamera />
     </>
   );
