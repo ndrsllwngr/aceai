96fa195310413f3d96aed374a67672603018ac54
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2020-02-12 12:44:25 +0100

    Convert svg to jsx
    https://transform.tools/svg-to-jsx

---
+++ site/src/images/svg/index.js
@@ -0,0 +1,39 @@
+import HeadLeftBodyLeft from './head-left-body-left';
+import HeadLeftBodyNeutral from './head-left-body-neutral';
+import HeadLeftBodyRight from './head-left-body-right';
+import HeadNeutralBodyLeft from './head-neutral-body-left';
+import HeadNeutralBodyNeutral from './head-neutral-body-neutral';
+import HeadNeutralBodyRight from './head-neutral-body-right';
+import HeadRightBodyLeft from './head-right-body-left';
+import HeadRightBodyNeutral from './head-right-body-neutral';
+import HeadRightBodyRight from './head-right-body-right';
+import HeightMinusDistanceMinus from './height-minus-distance-minus';
+import HeightMinusDistanceNeutral from './height-minus-distance-neutral';
+import HeightMinusDistancePlus from './height-minus-distance-plus';
+import HeightNeutralDistanceMinus from './height-neutral-distance-minus';
+import HeightNeutralDistanceNeutral from './height-netural-distance-neutral';
+import HeightNeutralDistancePlus from './height-neutral-distance-plus';
+import HeightPlusDistanceMinus from './height-plus-distance-minus';
+import HeightPlusDistanceNeutral from './height-plus-distance-neutral';
+import HeightPlusDistancePlus from './height-plus-distance-plus';
+
+export default {
+  HeadLeftBodyLeft,
+  HeadLeftBodyNeutral,
+  HeadLeftBodyRight,
+  HeadNeutralBodyLeft,
+  HeadNeutralBodyNeutral,
+  HeadNeutralBodyRight,
+  HeadRightBodyLeft,
+  HeadRightBodyNeutral,
+  HeadRightBodyRight,
+  HeightMinusDistanceMinus,
+  HeightMinusDistanceNeutral,
+  HeightMinusDistancePlus,
+  HeightNeutralDistanceMinus,
+  HeightNeutralDistanceNeutral,
+  HeightNeutralDistancePlus,
+  HeightPlusDistanceMinus,
+  HeightPlusDistanceNeutral,
+  HeightPlusDistancePlus,
+};
